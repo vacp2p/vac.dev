cd851ae586e24797650f04a1fda6c33f7202965a
Jakub Sokołowski <jakub@status.im>
2022-01-11 20:56:55 +0100

    reafactor Gulp config, fix minifying images and CSS
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,97 +1,99 @@
-import autoprefixer from "autoprefixer";
-import browserSync from "browser-sync";
-import spawn from "cross-spawn";
-import cssnano from "cssnano";
-import { dest, series, src, task, watch } from "gulp";
-import postcss from "gulp-postcss";
-import atimport from "postcss-import";
-import imagemin from "gulp-imagemin";
-import tailwindcss from "tailwindcss";
+import { dest, series, parallel, src, watch as gwatch } from 'gulp'
+import autoprefixer from 'autoprefixer'
+import browserSync from 'browser-sync'
+import spawn from 'cross-spawn'
+import cssnano from 'cssnano'
+import postcss from 'gulp-postcss'
+import atimport from 'postcss-import'
+import imagemin from 'gulp-imagemin'
+import minify from 'gulp-minify'
+import tailwindcss from 'tailwindcss'
 
-const SITE_ROOT = "./_site";
-const POST_BUILD_STYLESHEET = `${SITE_ROOT}/assets/css/`;
-const PRE_BUILD_STYLESHEET = "./assets/css/style.css";
-const IMAGES = "./assets/img";
-const IMAGES_MINIMIZED = `${SITE_ROOT}/assets/`;
-const TAILWIND_CONFIG = "./tailwind.config.js";
+const SITE_ROOT = './_site'
+const POST_BUILD_STYLESHEET = `${SITE_ROOT}/assets/css/`
+const PRE_BUILD_STYLESHEET = './assets/css/style.css'
+const IMAGES = './assets/img/**/*'
+const IMAGES_MINIMIZED = `${SITE_ROOT}/assets/img/`
+const TAILWIND_CONFIG = './tailwind.config.js'
 
 // Fix for Windows compatibility
-const jekyll = process.platform === "win32" ? "jekyll.bat" : "jekyll";
+const jekyll = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll'
 
-const isDevelopmentBuild = process.env.NODE_ENV === "development";
+const isDevelopmentBuild = process.env.NODE_ENV === 'development'
 
-task("buildJekyll", () => {
-  browserSync.notify("Building Jekyll site...");
+const content = () => {
+  browserSync.notify('Building Jekyll site...')
 
-  const args = ["exec", jekyll, "build"];
-
+  const args = ['exec', jekyll, 'build']
   if (isDevelopmentBuild) {
-    args.push("--incremental");
+    args.push('--incremental')
   }
 
-  return spawn("bundle", args, { stdio: "inherit" });
-});
+  return spawn('bundle', args, { stdio: 'inherit' })
+}
 
-task("processStyles", () => {
-  browserSync.notify("Compiling styles...");
+const scripts = () =>
+  src('./assets/js/*')
+    .pipe(minify({ext:{min:'.min.js' }, mangle: true}))
+    .pipe(dest(`${SITE_ROOT}/assets/js/`))
+    .pipe(browserSync.stream())
 
-  return src(PRE_BUILD_STYLESHEET)
+const styles = () =>
+  src(PRE_BUILD_STYLESHEET)
     .pipe(
-      postcss([
-        atimport(),
-        tailwindcss(TAILWIND_CONFIG),
-        ...(isDevelopmentBuild ? [] : [autoprefixer(), cssnano()]),
-      ])
-    )
-    .pipe(dest(POST_BUILD_STYLESHEET));
-});
+        postcss([
+          atimport(),
+          tailwindcss(TAILWIND_CONFIG),
+          ...(isDevelopmentBuild ? [] : [autoprefixer(), cssnano()]),
+        ])
+      )
+    .pipe(dest(POST_BUILD_STYLESHEET))
+    .pipe(browserSync.stream())
 
-task("cname", () => {
-  return src('assets/CNAME').pipe(dest(SITE_ROOT));
-});
+const cname = () =>
+  src('assets/CNAME').pipe(dest(SITE_ROOT))
 
-task("images", () => {
-  return src(IMAGES)
-    .pipe(
-      imagemin([
-        imagemin.gifsicle({ interlaced: true }),
-        imagemin.optipng({ optimizationLevel: 3 }),
-        imagemin.svgo(),
-      ])
-    )
+const images = () =>
+  src(IMAGES)
+    .pipe(imagemin([
+      imagemin.gifsicle({ interlaced: true }),
+      imagemin.optipng({ optimizationLevel: 3 }),
+      imagemin.svgo(),
+    ]))
+    .pipe(dest(IMAGES_MINIMIZED))
+    .pipe(browserSync.stream())
 
-    .pipe(dest(IMAGES_MINIMIZED));
-});
-
-task("startServer", () => {
+const server = () =>
   browserSync.init({
-    files: [SITE_ROOT + "/**"],
-    open: "local",
+    files: [SITE_ROOT + '/**'],
+    open: 'local',
     port: 4000,
     server: {
       baseDir: SITE_ROOT,
       serveStaticOptions: {
-        extensions: ["html"],
+        extensions: ['html'],
       },
     },
-  });
+  })
 
-  watch(
-    [
-      "**/*.css",
-      "**/*.html",
-      "**/*.js",
-      "**/*.md",
-      "**/*.markdown",
-      "!_site/**/*",
-      "!node_modules/**/*",
-    ],
-    { interval: 500 },
-    buildSite
-  );
-});
+const watch = () => {
+  gwatch(['_authors/*', '_data/*', '_posts/*', '_layouts/*', '_includes/*'], content)
+  gwatch('**/*.js', scripts)
+  gwatch(PRE_BUILD_STYLESHEET, styles)
+  gwatch(IMAGES, images)
+  //'!_site/**/*',
+  //'!node_modules/**/*',
+}
 
-const buildSite = series("buildJekyll", "processStyles", "images", "cname");
+const build = series(content, scripts, styles, images, cname)
 
-exports.serve = series(buildSite, "startServer");
-exports.default = series(buildSite);
+exports.content = content
+exports.scripts = scripts
+exports.styles = styles
+exports.images = images
+exports.cname = cname
+exports.watch = watch
+exports.server = server
+exports.build = build
+exports.devel = series(build, parallel(server, watch))
+exports.default = exports.build
