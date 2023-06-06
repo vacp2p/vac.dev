2942396d8c6a5ce00ba35b13ec7e04ab6f3b73e4
Hossein Mehrabi <ohyeaross@gmail.com>
2023-06-07 03:14:53 +0330

    feat: ugrade logos preset & enable author page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,6 +39,9 @@ const config = {
               default: {
                 content: {
                   authors,
+                  authorPage: {
+                    sidebar: 'main',
+                  },
                 },
               },
             },
