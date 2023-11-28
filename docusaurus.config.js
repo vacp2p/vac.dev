41f41e863326a9ec4b1c9468f675bb3e65fef99b
jinhojang6 <jinho5051@gmail.com>
2023-11-28 22:41:23 +0900

    style: use system theme by default

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -83,6 +83,10 @@ const config = {
   themeConfig:
     /** @type {import('@acid-info/logos-docusaurus-preset').ThemeConfig} */
     ({
+      colorMode: {
+        disableSwitch: false,
+        respectPrefersColorScheme: true,
+      },
       navbar: {
         items: [
           {
